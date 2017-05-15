74e64e1d35a624209ba3d542488fc616bc02ab6c
Falcort <falcort.jeux@gmail.com>
2017-05-15 10:36:38 +0200

    login panel

--- src/cpoa/CPOA.java
+++ src/cpoa/CPOA.java
@@ -5,6 +5,8 @@
  */
 package cpoa;
 
+import user_interface.login;
+
 /**
  *
  * @author Thinkpad-Falcort
@@ -15,7 +17,7 @@ public class CPOA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        login login = new login();
     }
     
 }
