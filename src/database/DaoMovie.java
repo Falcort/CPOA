e160be4ba99af0b358efccc245c2961c96137599
Falcort <falcort.jeux@gmail.com>
2017-06-01 11:42:42 +0200

    DAO Movie

---
+++ src/database/DaoMovie.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import tables.Movie;
+import java.util.List;
+
+/**
+ *
+ * @author Thinkpad-Falcort
+ */
+public class DaoMovie
+{
+    private final Connection connection;
+    
+    public DaoMovie(Connection connection)
+    {
+        this.connection = connection;
+    }
+    
+    public void recuprerMovie(List<Movie> movies) throws SQLException
+    {
+        String query = "SELECT * FROM FILM";
+        Statement stmt = connection.createStatement();
+        ResultSet rset = stmt.executeQuery(query);
+        while (rset.next())
+        {
+            int visa = rset.getInt(1);
+            String title = rset.getString(2);
+            String gender = rset.getString(3);
+            int date = rset.getInt(4);
+            Movie movie = new Movie();
+            movies.add(movie);
+        }
+        rset.close();
+        stmt.close();
+    }
+}
