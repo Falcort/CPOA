6ea720acdf1d2ba1c6e2ba47b98310834beb4503
Falcort <falcort.jeux@gmail.com>
2017-06-02 10:23:52 +0200

    Add and remove Movie DAO

--- src/database/DaoMovie.java
+++ src/database/DaoMovie.java
@@ -6,6 +6,7 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -41,5 +42,26 @@ public class DaoMovie
         }
         rset.close();
         stmt.close();
+    }
+    
+    public void insertMovie(Movie movie) throws SQLException
+    {
+        String query = "INSERT INTO FILM(numVisa, title, gender, releaseDate) VALUES (?,?,?,?)";
+        PreparedStatement pstmt = connection.prepareStatement(query);
+        pstmt.setInt(1, movie.getNumVisa());
+        pstmt.setString(2, movie.getTitle());
+        pstmt.setString(3, movie.getGenre());
+        pstmt.setInt(4, movie.getReleaseDate());
+        pstmt.executeUpdate();
+        pstmt.close();
+    }
+    
+    public void deleteMovie(int numMovie) throws SQLException
+    {
+        String query = "DELETE FROM FILM WHERE numVIP = ?";
+        PreparedStatement pstmt = connection.prepareStatement(query);
+        pstmt.setInt(1, numMovie);
+        pstmt.executeUpdate();
+        pstmt.close();
     }
 }
