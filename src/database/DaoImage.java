de32d29d16dc7be13fcc8c1e2a87ca008408f7d4
Etienne Fonteix <etienne.fonteix@gmail.com>
2017-06-14 15:58:45 +0200

    Ajout dao image

---
+++ src/database/DaoImage.java
@@ -0,0 +1,59 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import tables.Casting;
+
+public class DaoImage {
+private final Connection connexion;
+
+    public DaoImage(Connection connexion) {
+        this.connexion = connexion;
+    }
+
+        public void send (String fileName) {
+        String SFTPHOST = "falcort.fr";
+        int SFTPPORT = 2228;
+        String SFTPUSER = "etienne";
+        String SFTPPASS = "12345";
+        String SFTPWORKINGDIR = "/var/www/html/assets/images/VIP/";
+
+        Session session = null;
+        Channel channel = null;
+        ChannelSftp channelSftp = null;
+        System.out.println("preparing the host information for sftp.");
+        try {
+            JSch jsch = new JSch();
+            session = jsch.getSession(SFTPUSER, SFTPHOST, SFTPPORT);
+            session.setPassword(SFTPPASS);
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            session.setConfig(config);
+            session.connect();
+            System.out.println("Host connected.");
+            channel = session.openChannel("sftp");
+            channel.connect();
+            System.out.println("sftp channel opened and connected.");
+            channelSftp = (ChannelSftp) channel;
+            channelSftp.cd(SFTPWORKINGDIR);
+            File f = new File(fileName);
+            channelSftp.put(new FileInputStream(f), f.getName());
+            log.info("File transfered successfully to host.");
+        } catch (Exception ex) {
+             System.out.println("Exception found while tranfer the response.");
+        }
+        finally{
+
+            channelSftp.exit();
+            System.out.println("sftp Channel exited.");
+            channel.disconnect();
+            System.out.println("Channel disconnected.");
+            session.disconnect();
+            System.out.println("Host Session disconnected.");
+        }
+    }  
+}
