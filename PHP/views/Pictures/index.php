f0fd89dbb3e94cfc3a770e1b62b54febeef3f6e8
Falcort <falcort.jeux@gmail.com>
2017-06-17 14:33:15 +0200

    Ajout de row pour les image de VIP

--- PHP/views/Pictures/index.php
+++ PHP/views/Pictures/index.php
@@ -1,16 +1,26 @@
 <div class="center-align">
     <h1>VIPs Pictures</h1>
+    <div class="row center-align valign-wrapper">
+    <?php
+    for ($i = 0;
+    $i < count($viewModel);
+    $i++)
+    {
+    if ($i % 3 == 0)
+    { ?>
+</div>
+<div class="row center-align valign-wrapper">
+    <?php } ?>
 
-    <div class="col s12">
-        <?php foreach ($viewModel as $photo) : ?>
-            <form action="" type="POST">
-                <button type="submit" style="border: none; background: none;"><img class="responsive-img tooltipped" src="<?php echo $photo['wayPhoto']; ?>"
-                                           data-position="right"
-                                           data-delay="50"
-                                           data-tooltip="<?php echo($photo['yearPhoto'] . " - " . $photo['placePhoto'] . " : " . $photo['lastNameVIP'] . " " . $photo['firstNameVIP']); ?>">
-                    <input hidden type="text" value="<?php echo $photo['idPhoto']; ?>">
-                </button>
-            </form>
-        <?php endforeach; ?>
-    </div>
+    <form action="" type="POST" class="col s4">
+        <button type="submit" style="border: none; background: none;">
+            <img class="responsive-img tooltipped" src="<?php echo $viewModel[$i]['wayPhoto']; ?>"
+                 data-position="right"
+                 data-delay="50"
+                 data-tooltip="<?php echo($viewModel[$i]['yearPhoto'] . " - " . $viewModel[$i]['placePhoto'] . " : " . $viewModel[$i]['lastNameVIP'] . " " . $viewModel[$i]['firstNameVIP']); ?>">
+            <input hidden type="text" value="<?php echo $viewModel[$i]['idPhoto']; ?>">
+        </button>
+    </form>
+    <?php } ?>
+</div>
 </div>
